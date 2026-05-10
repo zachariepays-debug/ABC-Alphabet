6c278ed1a559cf3cd00e1f43a673a4b4b0da0cf3
zachariepays-debug <zachariepays@gmail.com>
2026-05-10 12:08:32 +0200

    Create ecole.py

---
+++ univers/ecole.py
@@ -0,0 +1,6 @@
+ECOLE_DATA = {
+    "Alphabet": list("ABCDEFGHIJKLMNOPQRSTUVWXYZ"),
+    "Chiffres": [str(i) for i in range(101)],
+    "Formes": {"🟦":"Carré","🔴":"Rond","🔺":"Triangle","⭐":"Étoile","💎":"Losange","❤️":"Cœur","🟩":"Rectangle"},
+    "Calculs": {"1+1":"2", "2+2":"4", "5+5":"10", "10+10":"20"}
+}
