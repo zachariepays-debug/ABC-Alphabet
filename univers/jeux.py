5ec4ca7cf119fe66a79420d566449e1cad54fdd5
zachariepays-debug <zachariepays@gmail.com>
2026-05-10 12:12:49 +0200

    Create jeux.py

---
+++ univers/jeux.py
@@ -0,0 +1,5 @@
+JEUX_DATA = {
+    "Musique": {"🎹":"Piano","🎸":"Guitare","🥁":"Batterie","🎺":"Trompette"},
+    "Émotions": {"😀":"Content","😢":"Triste","😡":"En Colère","😱":"Peur"},
+    "Cadeaux": {"🎁 CADEAU":"Confettis","🪄 MAGIE":"Neige","🎈 BALLONS":"Ballons"}
+}
