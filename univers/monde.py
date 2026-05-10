feb4fcff80f12aea85339f0b819dfecbf8021e6b
zachariepays-debug <zachariepays@gmail.com>
2026-05-10 12:12:27 +0200

    Create monde.py

---
+++ univers/monde.py
@@ -0,0 +1,5 @@
+MONDE_DATA = {
+    "Drapeaux": {"🇫🇷":"France","🇲🇦":"Maroc","🇩🇿":"Algérie","🇹🇳":"Tunisie","🇸🇳":"Sénégal","🇨🇮":"Côte d'Ivoire"},
+    "Transports": {"🚗":"Voiture","🚒":"Pompier","🚑":"Ambulance","🚓":"Police","🚀":"Fusée","✈️":"Avion"},
+    "Métiers": {"🧑‍⚕️":"Docteur","🧑‍🍳":"Cuisinier","🧑‍🚀":"Astronaute","👮":"Policier"}
+}
