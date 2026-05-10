6c589fe2c17581f57ab5abbd5037aa77e49ee810
zachariepays-debug <zachariepays@gmail.com>
2026-05-10 12:09:34 +0200

    Create nature.py

---
+++ univers/univers/nature.py
@@ -0,0 +1,5 @@
+NATURE_DATA = {
+    "Dinos": {"🦖 T-Rex":"Tyrannosaure","🦕 Diplo":"Diplodocus","🦕 Tricé":"Tricératops","🕊️ Ptéro":"Ptérodactyle"},
+    "Savane": {"🦁":"Lion","🐘":"Éléphant","🦒":"Girafe","🦓":"Zèbre","🐵":"Singe","🦛":"Hippo"},
+    "Océan": {"🐬":"Dauphin","🐋":"Baleine","🦈":"Requin","🐙":"Poulpe","🐢":"Tortue"}
+}
